589353694b8a7f28ca3f1b4cf559a87862bbc7d5
kganitis <ganitiskostas@gmail.com>
2025-06-29 19:28:24 +0300

    Add docker-compose file

--- Web/Program.cs
+++ Web/Program.cs
@@ -30,7 +30,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TicketingDemo API");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
